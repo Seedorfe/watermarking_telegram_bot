2e24f18e58b6721eda79251598093d3d41c4b961
Seedorfe <yourfriend1181@gmail.com>
2023-08-10 03:46:04 +0330

    main menu maked

--- src/bot.py
+++ src/bot.py
@@ -25,13 +25,41 @@ bot = app.bot
 async def start_command(inp : telegram.Update, context : ext.ContextTypes.DEFAULT_TYPE):
 
     if databace.get_admin_CHAT_ID() == 0:
-        _key_board = [[telegram.InlineKeyboardButton(text="شروع به عنوان ادمین", callback_data="set_admin")]]
-        await bot.send_message(inp.effective_message.chat_id, "برای شروع کار بر روی دکمه زیر کلیک کنید", 
-            reply_markup=telegram.InlineKeyboardMarkup(_key_board))
+
+        # _key_board = [[telegram.InlineKeyboardButton(text="شروع به عنوان ادمین", callback_data="set_admin")]]
+        # await bot.send_message(inp.effective_message.chat_id, "برای شروع کار بر روی دکمه زیر کلیک کنید", 
+        #     reply_markup=telegram.InlineKeyboardMarkup(_key_board))
+
+        _key_board = [[telegram.KeyboardButton(config.message_commands["set_admin"])]]
+
+        await bot.send_message(inp.effective_message.chat_id, "برای شروع کار بر روی دکمه کلیک کنید",
+            reply_markup = telegram.ReplyKeyboardMarkup(_key_board, resize_keyboard=True)
+        )
+            
+
     elif databace.get_admin_CHAT_ID() == inp.effective_message.chat_id:
-        await bot.send_message(inp.effective_message.chat_id, "سلام ادمین جون")
+
+
+        _key_board = [
+            [telegram.KeyboardButton(config.message_commands["process"]), telegram.KeyboardButton(config.message_commands["status"])],
+            [telegram.KeyboardButton(config.message_commands["options"]), telegram.KeyboardButton(config.message_commands["help"])]
+        ]
+
+
+        # await bot.send_message(inp.effective_message.chat_id, "سلام ادمین جون",
+        # reply_markup= telegram.ReplyKeyboardMarkup(_key_board, resize_keyboard=True)
+        # )
+
+        await bot.send_message(inp.effective_message.chat_id, config.bot_messages["main_menu"],
+        reply_markup=telegram.ReplyKeyboardMarkup(_key_board, resize_keyboard=True))
+        
+        
+ 
     else:
         pass # do nothing
+
+
+
 
 
 
@@ -41,6 +69,7 @@ async def button_quary_handler(inp : telegram.Update, context : ext.CallbackCont
         pass
     else:
         if inp.callback_query.data == "set_admin" and databace.get_admin_CHAT_ID() == 0:
+
             databace.set_admin_CHAT_ID(inp.effective_message.chat_id)
             await bot.send_message(inp.effective_message.chat_id, "شما ادمین شدید")
             
@@ -90,7 +119,7 @@ async def watermark(inp : telegram.Update, context : ext.ContextTypes.DEFAULT_TY
 async def video_handler(inp : telegram.Update, context : ext.ContextTypes.DEFAULT_TYPE):
 
     if databace.get_admin_CHAT_ID() == inp.effective_message.chat_id:
-        if len(init.get_files("/file")) < 3:
+        if len(init.get_files("/file")) < config.PROCESS_UPLOAD_FILE_LIMIT:
             await bot.send_message(inp.effective_message.chat_id, "فایل در حان دانلود")
             # await bot.send_message(inp.effective_message.chat_id, inp.message.effective_attachment.address)
             _file = await inp.message.effective_attachment.get_file()
@@ -101,7 +130,7 @@ async def video_handler(inp : telegram.Update, context : ext.ContextTypes.DEFAUL
             # await bot.send_message(inp.effective_message.chat_id, _file_name)
             await bot.send_message(inp.effective_message.chat_id, "فایل دانلود شد")
 
-            if len(init.get_files("/file")) >= 3:
+            if len(init.get_files("/file")) >= config.PROCESS_UPLOAD_FILE_LIMIT:
                 await bot.send_message(inp.effective_message.chat_id,"شما صد ویدعو آپلود کردید و بامحدودیت صد ویدعو مواجه شدید لطفا پردازش را شروع کنید")
 
         else:
@@ -115,7 +144,7 @@ async def video_handler(inp : telegram.Update, context : ext.ContextTypes.DEFAUL
 
 
 
-async def process_watermark(inp : telegram.Update, context : ext.ContextTypes.DEFAULT_TYPE):
+async def process_watermark_command(inp : telegram.Update, context : ext.ContextTypes.DEFAULT_TYPE):
 
     if databace.get_admin_CHAT_ID() == inp.effective_message.chat_id:
 
@@ -152,7 +181,41 @@ async def process_watermark(inp : telegram.Update, context : ext.ContextTypes.DE
         pass # do nothing
     
 
+async def text_message_handler(inp : telegram.Update, context : ext.ContextTypes.DEFAULT_TYPE):
 
+    if databace.get_admin_CHAT_ID() == inp.effective_message.chat_id:
+
+        if inp.message.text == config.message_commands["process"]:
+            await process_watermark_command(inp= inp, context= context)
+
+        elif inp.message.text == config.message_commands["options"]:
+
+            _key_board = [
+                [telegram.KeyboardButton(config.message_commands["watermark_options"])],
+                [telegram.KeyboardButton(config.message_commands["delete_videos"])],
+                [telegram.KeyboardButton(config.message_commands["cancel"])]
+            ]
+
+            await bot.send_message(inp.effective_message.chat_id, config.bot_messages["options_menu"],
+            reply_markup= telegram.ReplyKeyboardMarkup(_key_board, resize_keyboard= True)
+            )
+
+
+    else:
+        if inp.effective_message.text == config.message_commands["set_admin"] and databace.get_admin_CHAT_ID() == 0:
+            databace.set_admin_CHAT_ID(inp.effective_message.chat_id)
+
+            _key_board = [
+                [telegram.KeyboardButton(config.message_commands["process"]), telegram.KeyboardButton(config.message_commands["status"])],
+                [telegram.KeyboardButton(config.message_commands["options"]), telegram.KeyboardButton(config.message_commands["help"])]
+            ]
+
+            await bot.send_message(inp.effective_message.chat_id, config.bot_messages["admin_seted"],
+            reply_markup= telegram.ReplyKeyboardMarkup(_key_board, resize_keyboard=True)
+            )
+            
+            
+    
 
 
 
@@ -165,7 +228,8 @@ def run_bot():
     app.add_handler(ext.CommandHandler("start", start_command))
     app.add_handler(ext.CallbackQueryHandler(button_quary_handler))
     app.add_handler(ext.MessageHandler(ext.filters.VIDEO, video_handler))
-    app.add_handler(ext.CommandHandler("process", process_watermark))
+    app.add_handler(ext.CommandHandler("process", process_watermark_command))
+    app.add_handler(ext.MessageHandler(ext.filters.TEXT & ~ext.filters.COMMAND, text_message_handler))
     
 
     # bot = app.bot
--- src/config.py
+++ src/config.py
@@ -11,3 +11,26 @@ BOT_TOKEN = ""
 ROOT_PATH = ""
 
 
+
+PROCESS_UPLOAD_FILE_LIMIT = 100
+
+
+
+
+message_commands = {
+    "set_admin" : "شروع به عنوان ادمین",
+    "process" : "پردازش",
+    "options" : "تنظیمات",
+    "delete_videos": "حذف ویدعو های ذخیره شده",
+    "cancel" : "بیخیال",
+    "watermark_options": "تنظیمات واتر مارک",
+    "status":"وضعیت",
+    "help": "راهنما"
+}
+
+
+bot_messages = {
+    "admin_seted" : "شما ادمین شدید",
+    "main_menu": "منوی اصلی",
+    "options_menu" : "منوی تنظیمات"
+}
